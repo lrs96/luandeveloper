4b035ff5f344803192c22d9d2704aab780b947de
Luan rodrigues <luanrodrigues@kalunga.com.br>
2020-08-20 10:04:46 -0300

    Deploy do projeto web no netlify

---
+++ web/build/precache-manifest.679d82e31e273e0081592b65215d8af7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79394ef46445dc1710ca13d73a0bd7da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c837a22181dbecb59679",
+    "url": "/static/css/main.2a57a715.chunk.css"
+  },
+  {
+    "revision": "d1dfb08066ebb07fb47c",
+    "url": "/static/js/2.d44ddf39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d44ddf39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c837a22181dbecb59679",
+    "url": "/static/js/main.43800e3b.chunk.js"
+  },
+  {
+    "revision": "18ec23835bc88d959186",
+    "url": "/static/js/runtime-main.d8ce4a31.js"
+  },
+  {
+    "revision": "8de6fca21a6b65f42ae430cd13e51422",
+    "url": "/static/media/about-me-luan-rodrigues.8de6fca2.svg"
+  },
+  {
+    "revision": "0aff19be98c67e39f9810096b0d07666",
+    "url": "/static/media/luan-rodrigues-da-silva.0aff19be.svg"
+  }
+]);
